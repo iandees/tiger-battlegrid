d6ddf4f79005ba26e8ef288c2fecc656ef33ba31
Ian Dees <ian.dees@gmail.com>
2018-05-01 09:32:06 -0500

    Switch over to using linematch
    Follows https://github.com/mapbox/road-diff

--- index.js
+++ index.js
@@ -4,13 +4,11 @@ var tileReduce = require('@mapbox/tile-reduce');
 var path = require('path');
 
 tileReduce({
-  // bbox: [-87.8714, 41.7749, -87.4972, 41.9651],
-  tiles: [[1144, 1583, 12]],
   zoom: 12,
   map: path.join(__dirname, '/map.js'),
   sources: [
-    {name: 'osm', mbtiles: path.join('/Users/iandees/Downloads', 'united_states_of_america.mbtiles')},
-    {name: 'tiger', mbtiles: path.join('/Users/iandees/Downloads', 'tiger_roads.mbtiles')}
+    {name: 'osm', mbtiles: path.join('/mnt/osmqa', 'united_states_of_america.mbtiles'), raw: true},
+    {name: 'tiger', mbtiles: path.join('/mnt/osmqa', 'tiger2017_tiles.mbtiles'), raw: true}
   ]
 })
 .on('reduce', function(num) {
--- map.js
+++ map.js
@@ -1,89 +1,106 @@
 'use strict';
 
-var gju = require('geojson-utils');
-var createRuler = require('cheap-ruler');
-var tilebelt = require('@mapbox/tilebelt');
+var linematch = require('linematch');
+var lineclip = require('lineclip');
 
 module.exports = function(data, tile, writeData, done) {
-    var osm_highway_only = data.osm.osm.features.filter(function(f) {
-        return f.properties.highway && (
-            f.properties.highway == 'residential' ||
-            f.properties.highway == 'secondary' ||
-            f.properties.highway == 'primary' ||
-            f.properties.highway == 'tertiary' ||
-            f.properties.highway == 'motorway' ||
-            f.properties.highway == 'motorway_link')
-    });
 
-    data.osm.osm.features.forEach(function(f) {
-        if (f.properties.highway && (
-            f.properties.highway == 'residential' ||
-            f.properties.highway == 'secondary' ||
-            f.properties.highway == 'primary' ||
-            f.properties.highway == 'tertiary' ||
-            f.properties.highway == 'motorway' ||
-            f.properties.highway == 'motorway_link')) {
+  // filter and normalize input geometry
+  var tiger = toLines(data.tiger.tiger_roads);
+  var streets = toLines(data.osm.osm);
 
-            var feature = {
-                type: "Feature",
-                properties: {
-                    "highway": f.properties['highway'],
-                    "stroke": "#ff0000",
-                    "stroke-width": 4
-                },
-                geometry: f.geometry
-            }
-            writeData(JSON.stringify(feature) + '\n');
-        }
-    });
+  var diff;
+  if (tiger.length > 0 && streets.length > 0) {
+    // find tiger parts that are not covered by streets within 10 pixels;
+    // filter out chunks that are too short
+    diff = linematch(tiger, streets, 10).filter(filterShort);
+  } else if (tiger.length > 0) {
+    // The streets layer is empty and tiger has something
+    diff = tiger;
+  } else if (streets.length > 0) {
+    // The tiger layer is empty and streets has something
+    diff = streets;
+  } else {
+    // Both are empty
+    done(null, null);
+  }
 
-    var tiger_highway_only = data.tiger.tiger_roads.features.filter(function(f) {
-        return f.properties.MTFCC && (
-            f.properties.MTFCC == 'S1100' ||
-            f.properties.MTFCC == 'S1200' ||
-            f.properties.MTFCC == 'S1400' ||
-            f.properties.MTFCC == 'S1630')
-    });
+  if (diff.length) {
+    // write a feature with the diff as MultiLineString
+    // console.log(diff.length)
+    writeData(JSON.stringify({
+      type: 'Feature',
+      properties: {},
+      geometry: {
+        type: 'MultiLineString',
+        coordinates: toGeoJSON(diff, tile)
+      }
+    }) + "\n");
+  }
 
-    data.tiger.tiger_roads.features.forEach(function(f) {
-        if (f.properties.MTFCC &&
-           (f.properties.MTFCC == 'S1100' ||
-            f.properties.MTFCC == 'S1200' ||
-            f.properties.MTFCC == 'S1400' ||
-            f.properties.MTFCC == 'S1630')) {
-
-            var feature = {
-                type: "Feature",
-                properties: {
-                    "mtfcc": f.properties['MTFCC'],
-                    "stroke": "#0000ff",
-                    "stroke-width": 2
-                },
-                geometry: f.geometry
-            }
-            writeData(JSON.stringify(feature) + '\n');
-        }
-    });
-
-    var ruler = createRuler.fromTile(tile[1], tile[2]);
-
-    var osm_length = 0.0;
-    osm_highway_only.forEach(function(way) {
-        var len = ruler.lineDistance(way.geometry.coordinates);
-        if (len > 0) {
-            osm_length += len;
-        }
-    });
-
-    var tiger_length = 0.0;
-    tiger_highway_only.forEach(function(way) {
-        var len = ruler.lineDistance(way.geometry.coordinates);
-        if (len > 0) {
-            tiger_length += len;
-        }
-    });
-
-    // console.log({'tile': tile, 'osm': osm_length, 'tiger': tiger_length})
-
-    done(null, {'osm': osm_length, 'tiger': tiger_length});
+  done(null, null);
 };
+
+function toGeoJSON(diff, tile) {
+  var size = 4096 * Math.pow(2, tile[2]);
+  var x0 = 4096 * tile[0];
+  var y0 = 4096 * tile[1];
+
+  for (var i = 0; i < diff.length; i++) {
+    for (var j = 0; j < diff[i].length; j++) {
+      var p = diff[i][j];
+      var y2 = 180 - (p[1] + y0) * 360 / size;
+      diff[i][j] = [
+        round((p[0] + x0) * 360 / size - 180),
+        round(360 / Math.PI * Math.atan(Math.exp(y2 * Math.PI / 180)) - 90)];
+    }
+  }
+  return diff;
+}
+
+function round(num) {
+  return Math.round(num * 1e6) / 1e6;
+}
+
+function toLines(layer) {
+  var lines = [];
+  var bbox = [0, 0, 4096, 4096];
+
+  for (var i = 0; i < layer.length; i++) {
+    var feature = layer.feature(i);
+
+    // only consider polygon features with Tiger name or OSM highway tag
+    if (feature.type === 2 && (feature.properties.FULLNAME !== '' || feature.properties.highway)) {
+      var geom = feature.loadGeometry();
+
+      for (var k = 0; k < geom.length; k++) {
+        lineclip(normalizeLine(geom[k], layer.extent), bbox, lines); // clip to tile bbox and add to result
+      }
+    }
+  }
+  return lines;
+}
+
+function normalizeLine(line, extent) {
+  var newLine = [];
+  for (var i = 0; i < line.length; i++) {
+    newLine.push([
+      line[i].x * 4096 / extent,
+      line[i].y * 4096 / extent]);
+  }
+  return newLine;
+}
+
+function filterShort(line) {
+  return dist(line) >= 30; // line length is at least 30 pixels
+}
+
+function dist(line) { // approximate distance
+  var d = 0;
+  for (var i = 1; i < line.length; i++) {
+    var dx = line[i][0] - line[i - 1][0];
+    var dy = line[i][1] - line[i - 1][1];
+    d += Math.sqrt(dx * dx + dy * dy);
+  }
+  return d;
+}
